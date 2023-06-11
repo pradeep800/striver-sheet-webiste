f0ab9aa4547240f2f55d1348d5c83d8b1305c98b
pradeep <pradeep8b0@gmail.com>
2023-06-11 23:59:46 +0530

    remove confetti problems

--- lib/useUpdateRole.ts
+++ lib/useUpdateRole.ts
@@ -11,6 +11,7 @@ export default function useUpdateRole({ user }: { user?: SessionUser }) {
   //update will start working when you are authenticated
   const { update, status } = useSession();
   const setConfettiOn = useConfetti((state) => state.setConfettiOn);
+  const confettiOn = useConfetti((state) => state.confettiOn);
   const router = useRouter();
   const [firstTime, setFirstTime] = useState(false);
 
@@ -39,15 +40,17 @@ export default function useUpdateRole({ user }: { user?: SessionUser }) {
     }
     const interval = setInterval(UpdateAndRefresh, 1000 * 60 * 60);
     return () => clearInterval(interval);
-  }, [update, status, firstTime]);
+  }, [update, status, firstTime, confettiOn, user]);
   useEffect(() => {
-    const visibilityHandler = async () =>
+    const visibilityHandler = async () => {
       document.visibilityState === "visible" && UpdateAndRefresh();
+    };
 
     window.addEventListener("visibilitychange", visibilityHandler);
-    return () =>
+    return () => {
       window.removeEventListener("visibilitychange", visibilityHandler);
-  }, [update]);
+    };
+  }, [update, confettiOn, user]);
 
   return null;
 }
