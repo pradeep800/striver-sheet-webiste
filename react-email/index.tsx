0d36e1a3021228101d06930c262e6527efe3788f
pradeep <pradeep8b0@gmail.com>
2023-07-07 17:17:00 +0530

    remove some files

--- react-email/index.tsx
+++ react-email/index.tsx
@@ -1,18 +1,3 @@
-import { Button } from "@react-email/button";
-import { Html } from "@react-email/html";
-import * as React from "react";
-
 export default function Email() {
-  return (
-    <Html>
-      <Button
-        pX={20}
-        pY={12}
-        href="https://example.com"
-        style={{ background: "#000", color: "#fff" }}
-      >
-        Click me
-      </Button>
-    </Html>
-  );
+  return <div>hello</div>;
 }
