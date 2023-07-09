b1460e45052aa1620687a6d26ac0f04f620947ef
pradeep <pradeep8b0@gmail.com>
2023-07-09 16:59:26 +0530

    ..

--- app/(general)/[username]/error.tsx
+++ app/(general)/[username]/error.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-  return <div>error</div>;
+"use client";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={() => reset()}>Try again</button>
+    </div>
+  );
 }
