081f1edf473e3ebe36652e5426e8033dea5c5357
pradeep <pradeep8b0@gmail.com>
2023-07-09 16:31:52 +0530

    ..

--- app/example/page.tsx
+++ app/example/page.tsx
@@ -1,16 +1,7 @@
 "use client";
 import EmailTemplate from "@/components/emailTemplate";
 import Link from "next/link";
-import { Action } from "./seraction";
 
 export default function Example() {
-  return (
-    <div
-      onClick={async () => {
-        await Action();
-      }}
-    >
-      hello there
-    </div>
-  );
+  return <div>hello there</div>;
 }
