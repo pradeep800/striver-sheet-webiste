9ec5e14800ed0346509cbba48f35ded005f5d89b
pradeep <pradeep8b0@gmail.com>
2023-07-13 08:36:58 +0530

    .

---
+++ app/api/lc/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export function GET() {
+  const a = new Date();
+  const options = { timeZone: "Asia/Kolkata" };
+  const indianDate = a.toLocaleDateString("en-US", options);
+  return NextResponse.json(indianDate);
+}
