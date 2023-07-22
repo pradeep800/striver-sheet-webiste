560cc9b2538adf1d4e9e090ee968df64f6541a55
pradeep <pradeep8b0@gmail.com>
2023-07-22 10:41:37 +0530

    add home page

--- components/startToday.tsx
+++ components/startToday.tsx
@@ -8,11 +8,11 @@ const openSans = Open_Sans({
 export default function StartToday() {
   return (
     <section
-      className={`space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 min-h-[60vh] sm:min-h-[95vh]  flex items-center justify-center ${openSans.className}`}
+      className={`space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32 min-h-[60vh] sm:min-h-[90vh]  flex items-center justify-center ${openSans.className}`}
     >
       <div className="container flex max-w-[64rem]  flex-col items-center  gap-4 text-center">
         <h1 className="font-heading text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-red-500">
-          Practice dsa question in structure manner
+          Practice dsa questions in structure manner
         </h1>
         <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
           Level up your interview skills in 27 days with 191 frequently asked
