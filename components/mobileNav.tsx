bcfd997aeac239de821b597ba24c7ca5d00d0e18
pradeep <pradeep8b0@gmail.com>
2023-07-23 20:53:14 +0530

    change css in reminders

--- components/mobileNav.tsx
+++ components/mobileNav.tsx
@@ -79,7 +79,7 @@ export default function MobileNav({
               key={i}
             >
               <Link
-                className={`${
+                className={`py-1 ${
                   navItem.url !== activeNavLink
                     ? "relative hover:text-red-500 after:contain-[''] after:scale-x-0 after:origin-bottom-left after:bg-red-500 after:absolute after:transition-transform  after:bottom-0 after:left-0 after:h-[3px] after:w-[100%] hover:after:scale-x-[1] origin-bottom-right after:duration-500"
                     : " relative after:content-[''] after:bg-red-500 after:absolute  text-red-500 after:h-[3px] after:w-[100%] after:bottom-0 after:left-0 "
