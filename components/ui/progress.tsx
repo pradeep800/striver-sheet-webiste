ff6f90e2f9c8b987c17c4ef4d335166046a3f34c
pradeep <pradeep8b0@gmail.com>
2023-07-23 21:04:06 +0530

    add aria label

--- components/ui/progress.tsx
+++ components/ui/progress.tsx
@@ -10,6 +10,7 @@ const Progress = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
 >(({ className, value, ...props }, ref) => (
   <ProgressPrimitive.Root
+    aria-label="Progress Bar"
     ref={ref}
     className={cn(
       "relative h-4 w-full overflow-hidden rounded-full bg-secondary",
