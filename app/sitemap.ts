b46a090edf901f8219822ab5d24bbe8c623333df
pradeep <pradeep8b0@gmail.com>
2023-07-23 18:12:23 +0530

    .

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -6,7 +6,7 @@ export default async function sitemap() {
   const baseURL = env.NEXTAUTH_URL;
   const usersNames = await db.select({ userName: users.userName }).from(users);
   const allUserName = usersNames.map((userName) => {
-    return { url: `${baseURL}${userName.userName}`, lastModified: new Date() };
+    return { url: `${baseURL}/${userName.userName}`, lastModified: new Date() };
   });
   return [{ url: baseURL, lastModified: new Date() }, ...allUserName];
 }
