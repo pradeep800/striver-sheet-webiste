b0e6a61aba7aaecf04abe5e4245999026db14a97
pradeep <pradeep8b0@gmail.com>
2023-08-01 20:20:26 +0530

    trigger github action

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -57,7 +57,7 @@ export async function GET(req: Request) {
       ),
       {
         width: 1200,
-        height: 630,
+        height: 630, //action
         fonts: [
           {
             name: "asap",
