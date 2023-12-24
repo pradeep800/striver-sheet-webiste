cc446211b2b12e02155d89f561f85ee2c0db14a2
pradeep800 <pradeep8b0@gmail.com>
2023-12-24 12:48:23 +0530

    change url

--- components/chatContext.tsx
+++ components/chatContext.tsx
@@ -45,7 +45,7 @@ export const ChatContextProvider = ({ children, lambdaToken }: Props) => {
   const { mutate: sendMessage } = useMutation({
     mutationFn: async ({ message }: { message: string }) => {
       const url =
-        "https://6j5kkf3pnt63ne3lbvstl74qaa0hbnsd.lambda-url.ap-south-1.on.aws/";
+        "https://dxntuvslebdj6b2qu3dzerurkm0jibzd.lambda-url.ap-south-1.on.aws/";
       const headers = {
         "Content-Type": "text/plain",
         Authorization: "Bearer " + lambdaToken,
